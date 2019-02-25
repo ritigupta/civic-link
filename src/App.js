61cbd53911697d20bfe9e85a6186df7cf23dd537
Ritika Gupta <ritikag@uw.edu>
2019-02-24 21:41:17 -0800

    cards done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,161 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+//import CivicToolbar from './components/CivicToolbar';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import yellow from '@material-ui/core/colors/yellow';
+
+//import Typography from '@material-ui/core/Typography';
+
 import './App.css';
 
-import Button from '@material-ui/core/Button';
+
+const styles = theme => ({  
+  card: {
+    width: 301,
+    height: 492
+  },
+  media: {
+    height: 250,
+  },
+  cssRoot: {
+    color: theme.palette.getContrastText(yellow[600]),
+    backgroundColor: yellow[600],
+    '&:hover': {
+      backgroundColor: yellow[700],
+    },
+  }
+});
 
 class App extends Component {
+
   render() {
+    const { classes } = this.props;  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button variant="contained" color="primary">
-            Riti rules!
-          </Button>
-        </header>
+      <div className="App"> 
+        <body className="App-header">
+          <div>
+            <div class="flex-container">
+              <div class="flex-item">
+                <h1>Make an informed vote.</h1>
+                <h2>Scroll to meet your candidates.</h2>
+              </div>
+              <div class="flex-item">
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      style = {{ height: 0, paddingTop: '75%'}}
+                      image= {require ("./images/obama.JPG")}
+                      title="Barack Obama"
+                    />
+                    <CardContent>
+                      <h3>
+                        Barack Obama
+                      </h3>
+                      <h4>
+                        Democrat
+                      </h4>
+                      <p>
+                        <br></br>Affordable health care
+                        <br></br>Immigration reform
+                        <br></br>Renewable energy
+                      </p>          
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                      <Button 
+                        variant="contained" 
+                        size="medium"
+                        className={classes.cssRoot}>
+                        view full profile
+                      </Button>
+                  </CardActions>
+                </Card>
+              </div>
+              <div class="flex-item">
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      style = {{ height: 0, paddingTop: '75%'}}
+                      image= {require ("./images/weld.jpg")}
+                      title="William Weld"
+                    />
+                    <CardContent>
+                      <h3>
+                        William Weld
+                      </h3>
+                      <h4>
+                        Republican
+                      </h4>
+                      <p>
+                        <br></br>Marriage equity
+                        <br></br>Drug policy reform
+                        <br></br>Loosening gun control
+                      </p>          
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                      <Button 
+                        variant="contained" 
+                        size="medium"
+                        className={classes.cssRoot}>
+                        view full profile
+                      </Button>
+                  </CardActions>
+                </Card>
+              </div>
+              <div class="flex-item">
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      style = {{ height: 0, paddingTop: '75%'}}
+                      image= {require ("./images/amy.jpg")}
+                      title="Amy Klobuchar"
+                    />
+                    <CardContent>
+                      <h3>
+                        Amy Klobuchar
+                      </h3>
+                      <h4>
+                        Democrat
+                      </h4>
+                      <p>
+                        <br></br>Education reform
+                        <br></br>Renewable energy
+                        <br></br>Support for veterans
+                      </p>          
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                      <Button 
+                        variant="contained" 
+                        size="medium" 
+                        className={classes.cssRoot}>
+                        view full profile
+                      </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </body>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
