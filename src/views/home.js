d1645f96300b4816542055e717d82292d6cc9d25
Ritika Gupta <ritikag@uw.edu>
2019-03-05 18:09:14 -0800

    Fixed homepage typography

--- src/views/home.js
+++ src/views/home.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import MarchImage from "./images/march-for-our-lives.jpg";
+import SearchBar from "./components/SearchBar.js";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div class="home">
-          <h1 class="home">Our mission is to engage and inspire.</h1>
+        <div className="home">
+          <h1>Our mission is to engage and inspire.</h1>
           <br />
-          <h2>
+          <p className="mission">
             We wish to engage youth in the civic process through voter
             registration, voter education, and other activities and events that
             help them get involved in their communities and create lasting
             political change.
-          </h2>
+          </p>
+          <br />
+          <br />
+          <h5 className="event">
+            Search your city to find political events near you!
+          </h5>
+          <br />
+          <br />
+          <SearchBar />
         </div>
         <div>
           <img
